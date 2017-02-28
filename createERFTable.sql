4e80c4ec34a64274393b726232344cc0f5a22ae1
sutthipongl <suttihpong.lertmanorat@gmail.com>
2017-02-28 09:48:07 +0000

    Add secret column in SQL script

--- createERFTable.sql
+++ createERFTable.sql
@@ -1,6 +1,7 @@
 CREATE TABLE `ERF` (
     `filename` varchar(100) NOT NULL,
     `hash` varchar(65) DEFAULT NULL,
+    `secret` varchar(65) NOT NULL,
     `lastupdate` datetime DEFAULT NULL,
     PRIMARY KEY (`filename`)
   ) ;
