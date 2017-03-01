c094da2c48af2c7a3baa000014e77a5768e2d61f
sutthipongl <suttihpong.lertmanorat@gmail.com>
2017-03-01 21:41:42 +0000

    add Testcase1

---
+++ Testcases/Test1.sql
@@ -0,0 +1,22 @@
+/*
+-- Query: SELECT * FROM opennebula.ERF
+LIMIT 0, 1000
+
+-- Date: 2017-03-01 21:38
+*/
+INSERT INTO `ERF` (`filename`,`hash`,`secret`,`lastupdate`) VALUES ('/var/log/one/oned_20170301.log','d8f168f9064d3467488e67cc1ab0b70727ff7b74c84b297067aabc6f2a13a909','PNBm5N6aoA2F2v15jV96WZv1Nux2arjZdSmiGsq20sF2WH5dAcjwcPwXj3YrshqFa','2017-03-01 21:01:05');
+INSERT INTO `ERF` (`filename`,`hash`,`secret`,`lastupdate`) VALUES ('/var/log/one/oned_20170301_1.log','a45b73840a638c7f5b81184ff5fb1931d458fd930a1bad4597445980593c7027','JRMqaTxZ45HtQvulOeXlqCrk4VqdUEpBwc2E3xB7CjBsO3LCfIXFimYkhmx91VLv5','2017-03-01 21:03:34');
+INSERT INTO `ERF` (`filename`,`hash`,`secret`,`lastupdate`) VALUES ('/var/log/one/oned_20170301_2.log','f4e8feb5378a443061336936fc044edb89c6e28ff2ac268c5c0a3ca4e6be3b87','sMt9QYPClwj25j68KMc3vookK7myWRooDSxrQk3bRlbWEf2m1eqxAOPlT9TP1ibEa','2017-03-01 21:06:44');
+INSERT INTO `ERF` (`filename`,`hash`,`secret`,`lastupdate`) VALUES ('/var/log/one/oned_20170301_3.log','4490b41165f7a84a010a23019a0e0fbbaaa3b17bcca5804c81af92d32b8d6d6c','d0eFhVaGc3wntPzlcaPRsa65nRsvZN8bLnO3gWJq0dOr0lKdtz2WI813ZtyXgH622','2017-03-01 21:09:56');
+INSERT INTO `ERF` (`filename`,`hash`,`secret`,`lastupdate`) VALUES ('/var/log/one/oned_20170301_4.log','a831ea05899d90781ac489026e009450856eabbfb630d357f48b618083d2cba0','r9EAyDK34W0UsJzUhLEX869PfHnRolgPuSp1t74x13rtMZm1KYZQ46DiN07bjlYOb','2017-03-01 21:11:58');
+INSERT INTO `ERF` (`filename`,`hash`,`secret`,`lastupdate`) VALUES ('/var/log/one/oned_20170301_5.log','fa37a6d03412189cf3ca05e6f40d69ed7adc1e96f55e145ab4924b468cddee71','QHuuAddiy2U0QYYAHU1l9p2KMZsfWkZK0reyFqRdsJbgH7Qn2QI9dIRZIheCAbmAD','2017-03-01 21:15:07');
+INSERT INTO `ERF` (`filename`,`hash`,`secret`,`lastupdate`) VALUES ('/var/log/one/oned_20170301_6.log','8421688a1e09424508005a1e9441fb45ce9a42fb2fc28d8718d1ebef980ff9172','Wv6mpJROnUxjonUoN6IeDxRoT2iCEF9A8fXwWMijGNA2aupXy7abD1zw3R6FvfgBu','2017-03-01 21:18:54');
+INSERT INTO `ERF` (`filename`,`hash`,`secret`,`lastupdate`) VALUES ('/var/log/one/oned_20170301_7.log','63c4ad112ebc1c6890e4b774bd8c50aa717755f531650b229980cde8edfa4988','eiqMnT4rRZhUOaQQpwz5TcbLIuQ8UJw81USoNXOEW5wIdkyAQ7EIhNrZhh6bYAh0u','2017-03-01 21:20:48');
+INSERT INTO `ERF` (`filename`,`hash`,`secret`,`lastupdate`) VALUES ('/var/log/one/sched_20170301.log','81d943db88e861d48b74082adfaa0517ee3221c552b7d60212d7b7f2c8ff10cd','ipCxSaJYm0lnNnugQDrXxjucgdacVD2e0FLSNsO9t9wex0ulBUj9dNlu0uGW7G87j','2017-03-01 21:01:04');
+INSERT INTO `ERF` (`filename`,`hash`,`secret`,`lastupdate`) VALUES ('/var/log/one/sched_20170301_1.log','bc38378a734ee14d7ddca5d9744d689861db83790986b7004e33f15b08f089a4','y35l8J82zNZRq938lTZXVQ0fBc7QsQN0TRmZAs17f1ZD80LtRIrNwp26BaW4YI2Qz','2017-03-01 21:03:33');
+INSERT INTO `ERF` (`filename`,`hash`,`secret`,`lastupdate`) VALUES ('/var/log/one/sched_20170301_2.log','e1b269febaca4439de4772b9171aef0dc34cfdf1a930049c1ae748af9d957304','V5AbtzvVXOlfwS2vFPwhFmmIMUMhaoa5sIeVhHOdwas22vwGi2VXphEbcqqkNAnfg','2017-03-01 21:06:43');
+INSERT INTO `ERF` (`filename`,`hash`,`secret`,`lastupdate`) VALUES ('/var/log/one/sched_20170301_3.log','7a41914dec728a05484fc704f41d6750182558dd2bcf7eb97b1d51debb4220aa','GtZOqubDe6WciUbfTytUUts0Obzg5hDMIDy65HKiNGu4yDjrcKk6dM5ZXEe3TRNAs','2017-03-01 21:09:54');
+INSERT INTO `ERF` (`filename`,`hash`,`secret`,`lastupdate`) VALUES ('/var/log/one/sched_20170301_4.log','eb14f1b3acb8ad9df95b613167d1f0f9e1b9507b9e66a900e94daaead06877d0','X3WRSnkuhQ00bt0V3RdIQhbYXsW6jGLgHI5z3n1kd2iptgiu7tcYKlUFNQK7vvlcd','2017-03-01 21:11:58');
+INSERT INTO `ERF` (`filename`,`hash`,`secret`,`lastupdate`) VALUES ('/var/log/one/sched_20170301_5.log','5e9a3038d4222d0469228019950aa7c02fe99f0e8ea6ce03f24b29fd0deb8ac1','DZensMVQv97kWCJrJFcFDs8H0a9IBDrcAEz0qtQTCVbxyTWhy8U9B2OzcYhNzH09l','2017-03-01 21:15:14');
+INSERT INTO `ERF` (`filename`,`hash`,`secret`,`lastupdate`) VALUES ('/var/log/one/sched_20170301_6.log','5a745af6180732e3c3d0f392bceb6eb4c6e28491dda2dea82cf417ed96721994','uGF5PsFKrtKVm9Bl1PCgNe8F9Klo37PvNtAATejkH1d2bOncDXspbz4ihpFlwsQhV','2017-03-01 21:18:53');
+INSERT INTO `ERF` (`filename`,`hash`,`secret`,`lastupdate`) VALUES ('/var/log/one/sched_20170301_7.log','b0754ddfb61e4ddbc2aac985417181e21bc777f06949ea6f737a6799968e6a09','mHk2N5IQhmeB4enkFaHmj3W59QUWjeYDWgEJmkxBFJcJXx3AGLUZMR2WHVSZ9OA34','2017-03-01 21:21:03');
